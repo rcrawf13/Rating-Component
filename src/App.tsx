d05dbc593b4b8062f4d6ca711dea2a367efb9496
rcrawf13 <richmondcrawford44@gmail.com>
2025-11-03 12:44:07 -0500

    Added Modal Component & Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 import Stars from './components/Stars';
 
@@ -6,8 +6,36 @@ function App({heading = 'How Was Your Experience?'}) {
   const rating = Array.from({length:5},(_,index)=>index+1)
   const [hover, setHover] = useState(0);
   const [selectedRating, setSelectedRating] = useState(0)
+  const [isVisible, setIsVisible] = useState(false);
   const feedbackMessage = ['Terrible', 'Bad', 'Okay', 'Good', 'Excellent']
-  return (
+
+  const showModal = () => {
+    setIsVisible(true)
+
+}
+  
+
+  const closeModal = ():void => {
+    setSelectedRating(0);
+    setIsVisible(false)
+  }
+
+if (isVisible) {
+      
+  return(
+    <div className="focus">
+        <div className="modal">
+          <h2>You Rated Your Experience as:</h2>
+          <p className='response'>{feedbackMessage[selectedRating-1]}</p>
+        <button className='subButton' onClick={closeModal}>Close</button>
+        </div>
+
+      </div>
+  )
+}
+else {
+
+   return (
     <>
       <div className='rating-box'>
         <h1>{heading}</h1>
@@ -16,12 +44,18 @@ function App({heading = 'How Was Your Experience?'}) {
             style={selectedRating===1?({'color':'red'}):(selectedRating===5?({'color':'green'}):({'color':''}))}
             >{feedbackMessage[selectedRating-1]}
             </p>
-      </div>
 
+            <button className='subButton' onClick={showModal}>Submit</button>
+      </div>
 
     </>
 
   )
+
+}
+
+
+ 
 }
 
 export default App
